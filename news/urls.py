6dbe73d4df0a4a7ef03ce7f4b934b1d9917671ce
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-02-13 23:36:33 +0500

    Урок 11: исправили маршрут для получения каталога

--- news/urls.py
+++ news/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 # будет иметь префикс в urlах /news/
 urlpatterns = [
-    path('', views.get_all_news),
-    path('catalog/', views.catalog, name='catalog'),
+    path('catalog/', views.get_all_news, name='catalog'),
     path('catalog/<int:article_id>/', views.get_detail_article_by_id, name='detail_article_by_id'),
     path('catalog/<slug:title>/', views.get_detail_article_by_title, name='detail_article_by_title'),
 ]
