f9f8b10fee00b9b4cefe5c6e969dbb0aac931048
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-02-21 00:18:15 +0500

    Урок 14: добавили в админ. панель поле наличие слова пауки в содержании и дополнительные действия

--- news/admin.py
+++ news/admin.py
@@ -9,16 +9,6 @@ admin.site.site_title = "Админка"
 admin.site.index_title = "Привет админ! Не сломай ничего."
 
 
-def make_inactive(modeladmin, request, queryset):
-    queryset.update(is_active=False)
-
-def make_active(modeladmin, request, queryset):
-    queryset.update(is_active=True)
-
-make_inactive.short_description = 'Сделать неактивными выбранные статьи'
-make_active.short_description = 'Сделать активными выбранные статьи'
-
-
 class TagInline(admin.TabularInline):
     model = Article.tags.through
     extra = 1
@@ -26,7 +16,7 @@ class TagInline(admin.TabularInline):
 
 class ArticleAdmin(admin.ModelAdmin):
     # list_display отображает поля в таблице
-    list_display = ('pk', 'title', 'category', 'publication_date', 'views', 'colored_status')
+    list_display = ('pk', 'title', 'category', 'publication_date', 'views', 'has_spiders')
     # list_display_links позволяет указать в качестве ссылок на объект другие поля
     list_display_links = ('pk', 'title')
     # list_filter позволяет фильтровать по полям
@@ -36,7 +26,7 @@ class ArticleAdmin(admin.ModelAdmin):
     # search_fields позволяет искать по полям
     search_fields = ('title', 'content')
     # actions позволяет выполнять действия над выбранными записями
-    actions = (make_inactive, make_active)
+    actions = ('make_inactive', 'make_active', 'set_checked', 'set_unchecked')
     list_per_page = 20
     # fields позволяет выбирать поля для редактирования (не fieldsets)
     # fields = ('title', 'category', 'content', 'tags', 'is_active')
@@ -57,6 +47,28 @@ class ArticleAdmin(admin.ModelAdmin):
         return format_html('<span style="color: {};">{}</span>', 'green' if obj.is_active else 'red', obj.is_active)
 
     colored_status.short_description = 'Статус'
+
+    @admin.display(description='Пауки внутри')
+    def has_spiders(self, article):
+        return 'Да' if 'пауки' in article.content else 'Нет'
+
+    @admin.action(description='Сделать неактивными выбранные статьи')
+    def make_inactive(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description='Сделать активными выбранные статьи')
+    def make_active(modeladmin, request, queryset):
+        queryset.update(is_active=True)
+
+    @admin.action(description='Отметить статьи как проверенные')
+    def set_checked(self, request, queryset):
+        updated = queryset.update(status=Article.Status.CHECKED)
+        self.message_user(request, f'{updated} статей было отмечено как проверенные')
+
+    @admin.action(description='Отметить статьи как не проверенные')
+    def set_unchecked(self, request, queryset):
+        updated = queryset.update(status=Article.Status.UNCHECKED)
+        self.message_user(request, f'{updated} статей было отмечено как не проверенные', 'warning')
 
 
 admin.site.register(Article, ArticleAdmin)
