0a50a8c340d27f39bbf843c4e6af87d63d3a8483
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-10 23:48:44 +0500

    Урок 21: дополнили шаблон с построчным рендером полей ввода

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
+from .models import Category
+
 
 class ArticleForm(forms.Form):
     title = forms.CharField(label='Заголовок', max_length=255)
     content = forms.CharField(label='Содержание', widget=forms.Textarea)
+    category = forms.ModelChoiceField(
+        label='Категория',
+        queryset=Category.objects.all(),
+        required=True,
+        empty_label='Выберите категорию'
+    )
