4158ca964dfdd68ae27317a217e978f22a886b4b
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-12 23:33:35 +0500

    Урок 22: добавили поле для хранения изображений в статьях

--- news/models.py
+++ news/models.py
@@ -58,6 +58,13 @@ class Article(models.Model):
                                  choices=(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
                                  verbose_name='Проверено')
 
+    image = models.ImageField(
+        upload_to='articles/%Y/%m/%d/',
+        blank=True,
+        null=True,
+        verbose_name='Изображение'
+    )
+
     objects = ArticleManager()
     all_objects = AllArticleManager()
 
