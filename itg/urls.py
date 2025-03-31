27a5a196a64dd7af9c60a428b73210d84d201855
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-31 23:14:43 +0500

    Урок 29: оптимизировали код: все взаимодействия с пользователем через allauth

--- itg/urls.py
+++ itg/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 from itg import settings
 from news import views
+from users.views import CustomConfirmEmailView, CustomPasswordResetDoneView, CustomPasswordResetFromKeyView, CustomPasswordResetFromKeyDoneView, CustomPasswordResetView, CustomLoginView, CustomSignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,8 +12,28 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('news/', include('news.urls', namespace='news')),
     # path('users/', include('users.urls', namespace='users')),
+
+    # Переопределенные URL allauth
+    path('accounts/confirm-email/<str:key>/', CustomConfirmEmailView.as_view(), name='account_confirm_email'),
+    path('accounts/password/reset/key/<uidb36>/<str:key>/', CustomPasswordResetView.as_view(), name='account_reset_password_from_key'),
+    path('accounts/password/reset/key/done/', CustomPasswordResetFromKeyDoneView.as_view(), name='account_reset_password_from_key_done'),
+    path('accounts/password/reset/', CustomPasswordResetView.as_view(), name='account_reset_password'),
+    path('accounts/password/reset/done/', CustomPasswordResetDoneView.as_view(), name='account_reset_password_done'),
+    path('accounts/login/', CustomLoginView.as_view(), name='account_login'),
+    path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
+
+    # Общие URL allauth
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path(
+        'accounts/password/reset/key/q-set-password/',
+        CustomPasswordResetFromKeyView.as_view(),
+        kwargs={'uidb36': 'q', 'key': 'set-password'},
+        name='special_password_reset'
+    ),
+]
 
 if settings.DEBUG:
     import debug_toolbar
--- users/views.py
+++ users/views.py
@@ -1,22 +1,31 @@
-from allauth.account.views import ConfirmEmailView
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, reverse
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
-
-from .forms import CustomAuthenticationForm
+from allauth.account.views import ConfirmEmailView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView, PasswordResetView, LoginView, SignupView
 from news.views import BaseMixin
 
 
-class LogoutUser(BaseMixin, LogoutView):
-    next_page = reverse_lazy('account_login')
+# Кастомные представления с BaseMixin
+class CustomConfirmEmailView(BaseMixin, ConfirmEmailView):
+    template_name = 'account/confirm_email.html'
 
 
-class CustomConfirmEmailView(ConfirmEmailView):
-    def get(self, *args, **kwargs):
-        response = super().get(*args, **kwargs)
-        if self.object.emailaddress_set.filter(verified=True).exists():
-            return redirect('account_login')
-        return response
+class CustomPasswordResetDoneView(BaseMixin, PasswordResetDoneView):
+    template_name = 'account/password_reset_done.html'
+
+
+class CustomLoginView(BaseMixin, LoginView):
+    template_name = 'account/login.html'
+
+
+class CustomPasswordResetFromKeyView(BaseMixin, PasswordResetFromKeyView):
+    template_name = 'account/password_reset_from_key.html'
+
+
+class CustomPasswordResetFromKeyDoneView(BaseMixin, PasswordResetFromKeyDoneView):
+    template_name = 'account/password_reset_from_key_done.html'
+
+
+class CustomPasswordResetView(BaseMixin, PasswordResetView):
+    template_name = 'account/password_reset.html'
+
+
+class CustomSignupView(BaseMixin, SignupView):
+    template_name = 'account/signup.html'
